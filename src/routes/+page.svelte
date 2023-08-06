b109938f701e48b568b96b8f06a0053003c32946
Altuğ Bakan <mail@alt.ug>
2023-08-06 20:04:50 +0200

    Update captcha display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,7 @@
 	import { goto } from '$app/navigation';
 	import { PUBLIC_FAUCET_ADDRESS, PUBLIC_CAPTCHA_SITE_KEY } from '$env/static/public';
 	import { BANANO_REGEX, MONKEY_API_URL } from '$lib/constants';
+	import { onDestroy, onMount } from 'svelte';
 
 	let value = '';
 	let isFocused = false;
@@ -55,47 +56,56 @@
 		defer></script>
 </svelte:head>
 
-<div class="mx-auto w-3/4 max-w-md space-y-4 text-center">
-	<section>
-		<img
-			src="{MONKEY_API_URL}{PUBLIC_FAUCET_ADDRESS}"
-			alt="Faucet MonKey"
-			class="max-w-xs mx-auto aspect-square" />
-		<h1
-			class="text-3xl dark:bg-gradient-to-br dark:from-primary-500 dark:to-secondary-300 dark:bg-clip-text dark:text-transparent dark:box-decoration-clone">
-			Welcome to MonKey Faucet!
-		</h1>
-	</section>
-	<section class="border-t border-surface-500/30 p-4">
-		<label for="address" class="label text-left ml-2 mb-1 text-sm sm:text-lg">
-			Enter your Banano address to continue
-		</label>
-		<div
-			class="input-group flex rounded-container-base"
-			class:variant-soft-error={invalid}
-			class:variant-soft-success={!disabled}>
-			<input
-				bind:value
-				on:focus={onFocus}
-				on:blur={onBlur}
-				on:keydown={onKeydown}
-				class="bg-transparent min-w-0 border-0 ring-0 outline-none px-2 flex-1"
-				name="address"
-				id="address"
-				placeholder="ban_1xred...sw" />
-			<button on:click={redirect} {disabled} class="btn variant-filled-primary flex-none">
-				<i class="fa-solid fa-arrow-right" />
-			</button>
-		</div>
-	</section>
-	<!-- prettier-ignore -->
-	<footer>
-		No Banano address? You can grab a wallet
-		<a
-			class="text-primary-700 dark:text-primary-500 hover:underline"
-			href="https://banano.cc/#wallets"
-			target="_blank"
-			rel="noopener">
-			here</a>.
-	</footer>
+<div class="flex flex-col h-full">
+	<div class="mx-auto w-3/4 max-w-md space-y-4 justify-center text-center">
+		<section>
+			<img
+				src="{MONKEY_API_URL}{PUBLIC_FAUCET_ADDRESS}"
+				alt="Faucet MonKey"
+				class="max-w-xs mx-auto aspect-square" />
+			<h1
+				class="text-3xl dark:bg-gradient-to-br dark:from-primary-500 dark:to-secondary-300 dark:bg-clip-text dark:text-transparent dark:box-decoration-clone">
+				Welcome to MonKey Faucet!
+			</h1>
+		</section>
+		<section class="border-t border-surface-500/30 p-4">
+			<label for="address" class="label text-left ml-2 mb-1 text-sm sm:text-lg">
+				Enter your Banano address to continue
+			</label>
+			<div
+				class="input-group flex rounded-container-base"
+				class:variant-soft-error={invalid}
+				class:variant-soft-success={!disabled}>
+				<input
+					bind:value
+					on:focus={onFocus}
+					on:blur={onBlur}
+					on:keydown={onKeydown}
+					class="bg-transparent min-w-0 border-0 ring-0 outline-none px-2 flex-1"
+					name="address"
+					id="address"
+					placeholder="ban_1xred...sw" />
+				<button on:click={redirect} {disabled} class="btn variant-filled-primary flex-none">
+					<i class="fa-solid fa-arrow-right" />
+				</button>
+			</div>
+		</section>
+		<!-- prettier-ignore -->
+		<span>
+			No Banano address? You can grab a wallet
+			<a
+				class="text-primary-700 dark:text-primary-500 hover:underline"
+				href="https://banano.cc/#wallets"
+				target="_blank"
+				rel="noopener">
+				here</a>.
+		</span>
+	</div>
+	<div class="text-center text-xs mt-auto">
+		This site is protected by reCAPTCHA and the Google
+		<a class="underline" href="https://policies.google.com/privacy">Privacy Policy</a>
+		and
+		<a class="underline" href="https://policies.google.com/terms">Terms of Service</a>
+		apply.
+	</div>
 </div>
